d68b75bb3cff9da2b73bff2ecd43f7bfc5f87c57
Aminion <bonumsingum@gmail.com>
2019-08-19 22:08:09 +0300

    Fix shell.

--- release.nix
+++ release.nix
@@ -2,17 +2,18 @@ let
   pkgs = import <nixpkgs> { inherit config; };
 
   config = {
-    packageOverrides = pkgs: rec {
+    packageOverrides = pkgs: rec {    
       haskellPackages = pkgs.haskellPackages.override { overrides = haskOverrides; };
     };
   };
 
   haskOverrides = new: old: rec {
+    TaroTelegramBot = new.callPackage ./. {};
     telegram-api = new.callPackage ./derivations/telegram-api.nix {};
   };
 
 in{
   inherit pkgs;
-  packages = { inherit (pkgs.haskellPackages); };
+  packages = { inherit (pkgs.haskellPackages) TaroTelegramBot;};
 }
   
